3663193cdd72b44864e22640b28fae228f47c991
simonjoe246 <simonjoe246@gmail.com>
2019-09-01 10:50:02 +0800

    Service Interface design complete

---
+++ src/main/java/org/seckill/dto/SeckillExecution.java
@@ -0,0 +1,74 @@
+package org.seckill.dto;
+
+import org.seckill.entity.SuccessKilled;
+
+/**
+ * 封装执行秒杀后的结果
+ */
+public class SeckillExecution {
+
+    private long seckillId;
+
+    private long userPhone;
+
+    private int state;
+
+    private String stateInfo;
+
+    private SuccessKilled successKilled;
+
+    public SeckillExecution(long seckillId, long userPhone, int state, String stateInfo) {
+        this.seckillId = seckillId;
+        this.userPhone = userPhone;
+        this.state = state;
+        this.stateInfo = stateInfo;
+    }
+
+    public SeckillExecution(long seckillId, long userPhone, int state, String stateInfo, SuccessKilled successKilled) {
+        this.seckillId = seckillId;
+        this.userPhone = userPhone;
+        this.state = state;
+        this.stateInfo = stateInfo;
+        this.successKilled = successKilled;
+    }
+
+    public long getSeckillId() {
+        return seckillId;
+    }
+
+    public void setSeckillId(long seckillId) {
+        this.seckillId = seckillId;
+    }
+
+    public long getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(long userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public String getStateInfo() {
+        return stateInfo;
+    }
+
+    public void setStateInfo(String stateInfo) {
+        this.stateInfo = stateInfo;
+    }
+
+    public SuccessKilled getSuccessKilled() {
+        return successKilled;
+    }
+
+    public void setSuccessKilled(SuccessKilled successKilled) {
+        this.successKilled = successKilled;
+    }
+}
